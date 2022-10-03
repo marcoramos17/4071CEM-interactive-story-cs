8a72920a624a8cfc2f0e81c8afdd306b02820640
Nwachukwu1300 <85280990+Nwachukwu1300@users.noreply.github.com>
2022-10-03 16:28:22 +0100

    intro

--- junkyard.py
+++ junkyard.py
@@ -1,3 +1,8 @@
-print("WELCOME!")
+intro = """POST-APOCALYPTIC JUNKYARD 
+What happens when you’re run out of your job for being inefficient, despite being the hardest working employee you know? 
+What happens when afterwards you go out to find another job, but suddenly everything seems so cut off? Every company that
+you email sends back an automated reply, your calls are answered by speech bots, and every job centre you know of is
+ suddenly closed.  
+"""
 
-print("Hiiii my people")
+print(intro)

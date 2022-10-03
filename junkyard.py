ca6af3f5124d83593f2dc2d9ab8a9202fa401b26
Nwachukwu1300 <85280990+Nwachukwu1300@users.noreply.github.com>
2022-10-03 16:54:21 +0100

    int2

--- junkyard.py
+++ junkyard.py
@@ -2,7 +2,64 @@ intro = """POST-APOCALYPTIC JUNKYARD
 What happens when you’re run out of your job for being inefficient, despite being the hardest working employee you know? 
 What happens when afterwards you go out to find another job, but suddenly everything seems so cut off? Every company that
 you email sends back an automated reply, your calls are answered by speech bots, and every job centre you know of is
- suddenly closed.  
-"""
+suddenly closed.  
+This is my reality now. Our reality; mine and that of every other person living in this city. Over the course of months,
+every large corporation and business secretly ran their workers out of their careers. An incredible number of people
+and families were affected by their sudden total loss of income, with many turning to crime or sex work to get paid.  
+When every low-level employee was sacked, the corporations brought in their machines. Androids, supercomputers, all 
+sorts of artificial intelligence and robotic machinery had replaced – no, uprooted the job market. They operated at speeds
+and efficiencies that no human could ever hope to replicate. They didn’t need to be paid wages, only occasional maintenance
+fees. They were better than human workers in every single way, hence there was no use for us, they named them “Golems”. 
+Governments and corporations started to see impossibly large profit margins while the public were left to suffer. With 
+the lack of income combined with rising food prices and energy bills, we were forgotten about by those who were meant to
+care for us. The rich got richer, while the sick got sicker. It was an apocalypse, destroying the lives of millions. 
+We needed a new home, a new everything. And we found it. We created it. This is the Junkyard, an entirely new civilisation
+made by the common people for the common people. The Junkyard heavily reduced living taxes for all and did their best to
+ensure that all people had their basic needs met, but without large funding they struggled to provide. The Junkyard was
+keeping under the radar, away from the eyes of the corporate, knowing they would act if they knew that the Junkyard was 
+not being capitalised.  
+My name is Elon West, and I’m a nobody, just as everybody else is a nobody. I live in a single-floor house in the Junkyard
+and pay the rent by scavenging for materials. It’s hardly the career I used to have, but it’s a job and it pays. I go 
+beyond the borders of the Junkyard and search for any usable resources: scrap, supplies, anything that someone could use 
+back in the Junkyard. We’re a community: everyone knows everyone. We try to be welcoming to outsiders, but with the 
+looming threat of the corporate attempting to infiltrate us, we need to be careful. 
+I know quite a few people here, I see plenty of friends at bars and clubs in the nights, but there is a small circle of 
+people that I can truly confide in. The first is Tai, a mechanic and an absolute car nut that definitely has mental 
+problems. If you need a ride fixed up, he’s the man to go to, if you need muscle in a certain job, he is the man to go. 
+Often during the day he’s off-roading, kicking up dirt and mud miles away from here. At nights he often just shuts himself 
+away in his garage, working on God knows what.  
+There’s also Beth, the silver tongue of the group. Often, you’ll find her nicking some items from a store or generally 
+causing some minor havoc, yet she somehow always manages to talk her way out of trouble. Her natural charisma makes her 
+a great friend, and a terrifying enemy. The last person I know very well is Akima, a woman who knows a lot more about 
+computers than anyone else I know. She’s young, but a computer scientist with a degree in ethical hacking, though from 
+what I know the hacking she does nowadays is far from ethical. She’s gotten herself into camera feeds, bank accounts, 
+anywhere else that she shouldn’t be. Akima could have had a life living in the corporate world, but she once got too big 
+for her boots and attempted to hack into a major bank branch, immediately painting a target on her head. She takes refuge
+here now. 
+All four of us share a vision beyond the Junkyard. We want a society where we don’t have to work as much as we once did, 
+but still had the means to live comfortably. The employment market previously was corrupt, but it was better than not 
+having a market at all. Akima suggested we abandon any disregard for the law and get money illicitly, essentially stealing 
+from the corporate. Initially I didn’t know how I felt about that, but both Tai and Beth were heavily onboard with the 
+idea, so I was too. 
+They suggested that I speak with Kane, so that’s where I’m headed now. Kane is an officer who used to be on the side of 
+the corporate, but for some reason she defected and secretly joined us, acting as our spy. She never explained to anyone 
+why she betrayed them, and frankly I’m too afraid to ask. Nowadays Kane spends her days making sure that supplies 
+mysteriously disappear from corporate warehouses and end up in the Junkyard. I don’t know how she does it so consistently
+without being caught. 
+Somehow, Kane spots me before I spot her. I feel a hand grab my shoulder from behind and her somewhat gravelly voice speak,
+“Hey there, West. Haven’t seen you in a while.” I nod and turn around. She’s wearing her usual boring plain clothes, and
+her usual smirk on her face. “Hey, Kane. I was looking for you, actually.” Kane tilts her head in curiosity, and I explain 
+what my crew and I wanted to do. “You want to hit the corporate right where it hurts? Well, I don’t usually condone crime
+but it seems you’ve got a solid crew behind you. A driver, a hacker, a sweet talker. I can think of two things you could do.
+Either you could hit one of their banks and strike a blow against their economic security, or you could try targeting an
+important person. Kidnapping, assassination, depends what you’re willing to do and for how much. 
+I blink in surprise. “That sounds a little... extreme, especially for a first job. Do you not have anything smaller?” 
+Kane sighs. “You’re so boring. Fine, listen here. On the outskirts of the town, there’s a mechanic’s shop. Probably has 
+tons of scrap and tech you can salvage and use as you please. Or, if you feel like playing Good Samaritan, there’s a 
+corporate clinic not far away from there. Should have lots of food and medicine you can distribute to the folks at home.
+You won’t have time to hit both, though, you’ll have heat on you as soon as you’re done looting.” 
+I nod, “That sounds more like my thing.” 
+“Good. So what will it be: a scrap heist or a medicine raid?” 
 
+"""
 print(intro)
